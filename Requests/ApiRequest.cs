5b754366ed76b6e4428c2a9543ab0e0515cb056a
Slavik Shudenko <shudenkoslavik@gmail.com>
2021-10-24 16:32:18 +0300

    Create ApiRequest

---
+++ Requests/ApiRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using UniversityApiClient.Responses;
+
+namespace UniversityApiClient.Requests
+{
+    public abstract class ApiRequest<T> where T : ApiResponse
+    {
+        public abstract string RelativePath { get; set; }
+
+        public abstract Task<T> Execute(string endPoint);
+    }
+}
